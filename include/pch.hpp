f92d41e8a6911c99abcd73f03f9567f1dd78f373
Dimitri Lilov <dev.dmtrllv@gmail.com>
2022-12-06 13:53:19 +0100

    got app working but SDL not...

---
+++ include/pch.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+// ----------------  WINDOWS  ---------------- //
+#ifdef _WIN32
+
+#define NOMINMAX
+
+#include <SDKDDKVer.h>
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <io.h>
+#include <fcntl.h>
+#include <shellapi.h>
+#include <dwmapi.h>
+
+#ifndef DWMWA_USE_IMMERSIVE_DARK_MODE
+#define DWMWA_USE_IMMERSIVE_DARK_MODE 20
+#endif
+
+#pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "dbghelp.lib")
+#pragma comment(lib, "shlwapi.lib")
+
+#endif
+// ----------------  WINDOWS  ---------------- //
+
+
+
+// -----------  STANDARD INCLUDES  ----------- //
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <errno.h>
+#include <thread>
+#include <cassert>
+#include <concepts>
+#include <fstream>
+#include <streambuf>
+#include <filesystem>
+#include <optional>
+#include <queue>
+#include <set>
+#include <limits>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <semaphore>
+#include <unordered_map>
+// -----------  STANDARD INCLUDES  ----------- //
+
+
+
+// -------------  SDL & OpenGL -------------- //
+#include "SDL.h"
+// -------------  SDL & OpenGL -------------- //
+
+
+
+// ----------  Typedefs & Macros ------------ //
+using Path = std::filesystem::path;
+
+#undef ERROR // name colission
+// ----------  Typedefs & Macros ------------ //
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,165 @@
-﻿#include <stdio.h>
+﻿#include "pch.hpp"
+#include "Logger.hpp"
 
-int main()
+int appEntry([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 {
-	puts("Hello\n");
+	DarkDescent::Logger& logger = DarkDescent::Logger::get();
+
+	logger.info("Hello, World!");
+	MessageBox(NULL, L"Test", L"Helloa", MB_OK);
+
+	DarkDescent::Logger::terminate();
 	return 0;
 }
+
+#ifdef _WIN32
+void setupWinConsole(bool bindStdIn, bool bindStdOut, bool bindStdErr);
+
+int WINAPI wWinMain([[maybe_unused]] HINSTANCE hInstance, [[maybe_unused]] HINSTANCE hPrevInstance, [[maybe_unused]] PWSTR pCmdLine, [[maybe_unused]] int nCmdShow)
+{
+	setupWinConsole(true, true, true);
+	int argc = 0;
+	wchar_t** argv = CommandLineToArgvW(pCmdLine, &argc);
+
+	std::vector<std::string> argvList;
+
+	char buffer[512];
+
+	for (size_t i = 0; i < argc; i++)
+	{
+		const wchar_t* arg = argv[i];
+		size_t converted = 0;
+		errno_t err = wcstombs_s(&converted, buffer, arg, 512);
+		if (err != 0)
+			return err;
+
+		argvList.emplace_back(std::string(buffer));
+	}
+
+	std::vector<char*> parsedArgv;
+
+	for (size_t i = 0; i < argc; i++)
+		parsedArgv.emplace_back(argvList[i].data());
+
+	const int exitCode = appEntry(argc, parsedArgv.data());
+
+	LocalFree(argv);
+
+	return exitCode;
+}
+#else
+int main(int argc, char* argv[])
+{
+	return appEntry(argc, argv);
+}
+#endif
+
+#ifdef _WIN32
+void setupWinConsole(bool bindStdIn, bool bindStdOut, bool bindStdErr)
+{
+	// if (!AttachConsole(ATTACH_PARENT_PROCESS))	// try to hijack existing console of command line
+	AllocConsole();							// or create your own.
+
+	if (bindStdIn)
+	{
+		FILE* dummyFile;
+		freopen_s(&dummyFile, "CONIN$", "r", stdin);
+	}
+	if (bindStdOut)
+	{
+		FILE* dummyFile;
+		freopen_s(&dummyFile, "CONOUT$", "w", stdout);
+	}
+	if (bindStdErr)
+	{
+		FILE* dummyFile;
+		freopen_s(&dummyFile, "CONOUT$", "w", stderr);
+	}
+
+	// Redirect unbuffered stdin from the current standard input handle
+	if (bindStdIn)
+	{
+		HANDLE stdHandle = GetStdHandle(STD_INPUT_HANDLE);
+		if (stdHandle != INVALID_HANDLE_VALUE)
+		{
+			int fileDescriptor = _open_osfhandle((intptr_t)stdHandle, _O_TEXT);
+			if (fileDescriptor != -1)
+			{
+				FILE* file = _fdopen(fileDescriptor, "r");
+				if (file != NULL)
+				{
+					int dup2Result = _dup2(_fileno(file), _fileno(stdin));
+					if (dup2Result == 0)
+					{
+						setvbuf(stdin, NULL, _IONBF, 0);
+					}
+				}
+			}
+		}
+	}
+
+	// Redirect unbuffered stdout to the current standard output handle
+	if (bindStdOut)
+	{
+		HANDLE stdHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+		if (stdHandle != INVALID_HANDLE_VALUE)
+		{
+			int fileDescriptor = _open_osfhandle((intptr_t)stdHandle, _O_TEXT);
+			if (fileDescriptor != -1)
+			{
+				FILE* file = _fdopen(fileDescriptor, "w");
+				if (file != NULL)
+				{
+					int dup2Result = _dup2(_fileno(file), _fileno(stdout));
+					if (dup2Result == 0)
+					{
+						setvbuf(stdout, NULL, _IONBF, 0);
+					}
+				}
+			}
+		}
+	}
+
+	// Redirect unbuffered stderr to the current standard error handle
+	if (bindStdErr)
+	{
+		HANDLE stdHandle = GetStdHandle(STD_ERROR_HANDLE);
+		if (stdHandle != INVALID_HANDLE_VALUE)
+		{
+			int fileDescriptor = _open_osfhandle((intptr_t)stdHandle, _O_TEXT);
+			if (fileDescriptor != -1)
+			{
+				FILE* file = _fdopen(fileDescriptor, "w");
+				if (file != NULL)
+				{
+					int dup2Result = _dup2(_fileno(file), _fileno(stderr));
+					if (dup2Result == 0)
+					{
+						setvbuf(stderr, NULL, _IONBF, 0);
+					}
+				}
+			}
+		}
+	}
+
+	// Clear the error state for each of the C++ standard stream objects. We need to do this, as attempts to access the
+	// standard streams before they refer to a valid target will cause the iostream objects to enter an error state. In
+	// versions of Visual Studio after 2005, this seems to always occur during startup regardless of whether anything
+	// has been read from or written to the targets or not.
+	if (bindStdIn)
+	{
+		std::wcin.clear();
+		std::cin.clear();
+	}
+	if (bindStdOut)
+	{
+		std::wcout.clear();
+		std::cout.clear();
+	}
+	if (bindStdErr)
+	{
+		std::wcerr.clear();
+		std::cerr.clear();
+	}
+}
+#endif
